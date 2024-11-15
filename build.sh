91e2edd1e0923326d146476aee80d76e6ab14705
CrisOrtiz0 <cdos03@educa.madrid.org>
2024-11-16 00:44:03 +0100

    Build mejorado, index y paginas hechas :D

--- build.sh
+++ build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+
 W3CSS_URL="https://www.w3schools.com/w3css/4/w3.css"
 CSS_DIR="css"
 W3CSS_FILE="$CSS_DIR/w3.css"
+DIST_DIR="../dist"
 
 mkdir -p $CSS_DIR
 
 if [ ! -f "$W3CSS_FILE" ]; then
     echo "Descargando w3.css..."
-
     curl -o "$W3CSS_FILE" "$W3CSS_URL"
-
+    
     if [ $? -ne 0 ]; then
         echo "Error: No se pudo descargar w3.css"
         exit 1
@@ -18,4 +19,10 @@ else
     echo "w3.css ya está descargado"
 fi
 
-mkdir -p ../dist
+mkdir -p $DIST_DIR
+
+echo "Copiando archivos al directorio dist..."
+cp *.html $DIST_DIR/
+cp -r $CSS_DIR $DIST_DIR/
+
+echo "Sitio web construido exitosamente en el directorio dist"
